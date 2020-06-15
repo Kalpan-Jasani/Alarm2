801cbca034dc9f8ed854f8fa100a1b4b30b2817c
Kalpan Jasani <kjasani@purdue.edu>
2020-06-15 18:19:29 -0400

    update bad package lock file

--- app/App.js
+++ app/App.js
@@ -11,6 +11,7 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to React Native!</Text>
       <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>"Hello World!", says Alarm²</Text>
       <Text style={styles.instructions}>{instructions}</Text>
     </View>
   );
