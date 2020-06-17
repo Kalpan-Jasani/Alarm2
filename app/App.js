4c0b658eb0eb37e1bfdc2b2c3f9b863ac1c10530
Sneha Basu <basu13@purdue.edu>
2020-06-16 20:38:56 -0400

    Update App.js

--- app/App.js
+++ app/App.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View , Switch} from 'react-native';
+import { useState } from "react";
+
+  
+
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -7,12 +11,21 @@ const instructions = Platform.select({
 });
 
 export default function App() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to React Native!</Text>
       <Text style={styles.instructions}>To get started, edit App.js</Text>
       <Text style={styles.instructions}>"Hello World!", says Alarm²</Text>
       <Text style={styles.instructions}>{instructions}</Text>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 }
